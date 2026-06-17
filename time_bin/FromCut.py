4d514978d3335c37652798041edb7cde56b6e92b
Ry0no0t0 <ryojohn0731@gmail.com>
2026-06-17 17:17:59 +0200

    started python file. Only user input.

--- time_bin/FromCut.py
+++ time_bin/FromCut.py
@@ -7,5 +7,33 @@ import pandas as pd
 import seaborn as sns
 import os
 import sys
+import time
 from datetime import timedelta
 
+#start the time counter
+start = time.time()
+
+def format_time:
+
+
+#User arguments
+if len(sys.argv) not in (3, 4):
+    print("User argument must include \n 1. Name of the file of the trace \n" \
+    "2. Name of output folder \n (3. Number of units trimmed at the beginning and at the end of trace: Default 6000)")
+    sys.exit(1)
+
+trace_name = sys.argv[1]
+foldername = sys.argv[2]
+dirname = "results/"+foldername
+
+if len(sys.argv) == 4:
+    trim = sys.argv[3]
+else:
+    trim = 6000
+
+print("Output Directory %s" % dirname, flush=True)
+if os.path.exists(dirname) == False:
+    os.mkdir(dirname)
+
+#Read trace
+traces = pd.read_csv(trace_name, delimiter="\t")
