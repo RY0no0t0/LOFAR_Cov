fa05fcfa3b8e803597bbd6038ea443b780633aa9
Ry0no0t0 <ryojohn0731@gmail.com>
2026-06-18 12:31:53 +0200

    Added full trace plot and started covariance matrix plot

--- time_bin/FromCut.py
+++ time_bin/FromCut.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from matplotlib.patches import Rectangle
+from matplotlib.backends.backend_pdf import PdfPages
 import pandas as pd
 import seaborn as sns
 import os
@@ -36,17 +37,64 @@ def format_time(sec):
 
     return " ".join(parts)
 
-def draw_FullTrace(trace):
-    fig, axes = plt.subplots(nrows=7, ncols=1, figsize=(12,12), sharey=True)
+def draw_FullTrace(trace, dirname):
+    plt.figure()
 
-    for i in range(6):
+    rows = len(trace)//10000+1
+    fig, axes = plt.subplots(nrows=rows, ncols=1, figsize=(12,2*rows), sharey=True)
+
+    for i in range(rows-1):
         axes[i].plot(traces[10000*i:10000*(i+1)])
-    axes[6].plot(np.concatenate((traces[60000:], np.zeros(70000-len(traces))), axis=None))
+    axes[-1].plot(np.concatenate((traces[(rows-1)*10000], np.zeros(rows*10000-len(traces))), axis=None))
 
-    rect = Rectangle((len(traces)-60000, -0.001), (70000-len(traces)),0.002, fc="lightgray")
-    axes[6].add_patch(rect)
+    rect = Rectangle((len(traces)-(rows-1)*10000, -0.001), (rows*10000-len(traces)), 0.002, fc="lightgray")
+    axes[-1].add_patch(rect)
+
+    fn = os.path(dirname, "full_trace.pdf")
+    plt.savefig(fn, format="pdf")
+    plt.close()
+
+def draw_cov():
+    tc = traces[6000:-6000].reset_index(drop=True).values.flatten()
+    maxamp = np.abs(tc).max()
+    ratios = np.array([2.3, 2.7, 3.1, 3.5])
+    cuts = maxamp/ratios
+    durs = np.array([200, 500, 1000, 1500])
+
+    cutsN = len(cuts)
+    dursN = len(durs)
+
+    fig, axes = plt.subplots(nrows=cutsN, ncols=dursN, figsize=(5*dursN, 4*cutsN))
+
+    for i in range(cutsN):
+        for j in range(dursN):
+            cov, N = make_cov(tc, cuts[i], durs[j])
+            sns.heatmap(cov, xticklabels=False, yticklabels=False, ax=axes[i,j], square=True, cmap="bwr", norm=colors.CenteredNorm())
+            # if N != 0:
+            #     sns.heatmap(cov, xticklabels=False, yticklabels=False, ax=axes[i,j], square=True, cmap="bwr", norm=colors.CenteredNorm())
+            # else:
+            #     sns.heatmap(cov, xticklabels=False, yticklabels=False, ax=axes[i,j], square=True, cmap=["white"], linecolor="lightgray", linewidths=1, cbar=False)
+            axes[i,j].set_title(f"N={N}")
+
+            if i==cutsN-1:
+                axes[cutsN-1, j].set_xlabel(f"duration={durs[j]} units")
+
+        axes[i,0].set_ylabel(f"cut=maxamp/{ratios[i]}")
 
     plt.show()
+    plt.close()
+
+    fig, axes = plt.subplots(nrows=cutsN, ncols=1, figsize=(12, 2*cutsN))
+
+    for i in range(cutsN):
+        axes[i].plot(tc)
+        axes[i].axhline(y=cuts[i], color="tab:red")
+        axes[i].axhline(y=-cuts[i], color="tab:red")
+        axes[i].set_ylabel(f"cut=maxamp/{ratios[i]}")
+
+    plt.show()
+    plt.close()
+
 
 
 #User arguments
@@ -71,11 +119,14 @@ if os.path.exists(dirname) == False:
 #Read trace
 traces = pd.read_csv(trace_name, delimiter="\t")
 
+# Plot the full trace
+draw_FullTrace(traces, dirname)
 
 # Print time
 middle = time.time()
 elapsed = middle-start
-print(f"Set up MCMC: {elapsed}", flush=True)
+print(f"Making Covariance Matrix: {elapsed}", flush=True)
 fn_time = os.path.join(dirname, "time.txt")
 with open(fn_time, 'w') as f:
-    f.write(f"Set up MCMC: {elapsed}\n")
+    f.write(f"Making Covariance Matrix: {elapsed}\n")
+
